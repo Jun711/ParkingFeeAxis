d4fa472656da1937e7655f32f84ba9fcee1070d3
Jun <cygoh@umich.edu>
2017-11-25 21:04:58 -0800

    add stylesheet

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import {AppRegistry, Text, View} from 'react-native';
 
 import Component1 from './app/components/Component1/Component1';
+import Component2 from './app/components/Component2/Component2';
 
 export default class myapp extends Component {
   render() {
     return(
       <View>
-        <Component1 message="Hello World"/>
+        <Component1 />
+        <Component2 />
       </View>
     );
   }
---
+++ app/components/Component2/Component2.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text, View, StyleSheet} from 'react-native';
+
+
+export default class Component2 extends Component {
+  render() {
+    return(
+      <View>
+        <View style={styles.myView}>
+          <Text style={styles.myText}>Hello Brad</Text>
+        </View>
+        <View style={styles.container}>
+          <View style={styles.v1}>
+            <Text>View 1</Text>
+          </View>
+          <View style={styles.v2}>
+            <Text>View 2</Text>
+          </View>
+          <View style={styles.v3}>
+            <Text style={styles.vText3}>View 3</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  myView: {
+    backgroundColor: 'blue'
+  },
+  myText: {
+    color: 'white'
+  },
+  container: {
+    // flex container
+    flexDirection: 'row',
+    height: 100
+  },
+  v1: {
+    flex: 1,
+    backgroundColor: 'red',
+    padding: 10
+  },
+  v2: {
+    flex:1,
+    backgroundColor: 'blue',
+    padding: 10
+  },
+  v3: {
+    flex:1,
+    backgroundColor: 'yellow',
+    padding: 10
+  },
+  vText3: {
+    color: 'white'
+  }
+});
+
+AppRegistry.registerComponent('Component2', () => Component2);

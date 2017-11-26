cfc4ad0ca70dd8938924eabebb1a90cdd81b61ac
Jun <cygoh@umich.edu>
2017-11-25 18:53:43 -0800

    add a cross-platform component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {AppRegistry, Text, View} from 'react-native';
 
+import Component1 from './app/components/Component1/Component1';
+
 export default class myapp extends Component {
   render() {
     return(
       <View>
-        <Text>Hello World!</Text>
+        <Component1 />
       </View>
     );
   }

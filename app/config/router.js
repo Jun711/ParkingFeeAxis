ae38922f734d067e6ba8d78a708f53ce51665880
Jun <cygoh@umich.edu>
2017-11-27 17:55:27 -0800

    embedded stackNavigator within tabNavigator

--- app/config/router.js
+++ app/config/router.js
@@ -27,15 +27,6 @@ import Me from '../screens/Me';
 //   }
 // })
 
-export const Tabs = TabNavigator({
-  Feed: {
-    screen: Feed,
-  },
-  Me: {
-    screen: Me,
-  }
-})
-
 export const FeedStack = StackNavigator({
   Feed: {
     screen: Feed,
@@ -45,8 +36,26 @@ export const FeedStack = StackNavigator({
   },
   Details: {
     screen: UserDetail,
-    navigationOptions: ({navigaation}) => ({
+    navigationOptions: ({navigation}) => ({
       title: `${navigation.state.params.name.first.toUpperCase()} ${navigation.state.params.name.last.toUpperCase()}`
     }),
   }
 });
+
+export const Tabs = TabNavigator({
+  Feed: {
+    screen: FeedStack,
+    navigationOptions: {
+      tabBarLabel: 'Feed',
+      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />,
+    }
+  },
+  Me: {
+    screen: Me,
+    navigationOptions: {
+      tabBarLabel: 'Me',
+      tabBarIcon: ({ tintColor }) => <Icon name="account-circle" size={35} color={tintColor} />,
+    }
+  }
+})
+

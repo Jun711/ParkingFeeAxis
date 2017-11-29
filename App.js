8a0e950e545c8754690e0fbdd411a66f3df93d76
Jun <cygoh@umich.edu>
2017-11-28 23:47:57 -0800

    added reactNativeMaps

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {AppRegistry, View, Text} from 'react-native';
+import {AppRegistry, View, Text, StyleSheet, Dimensions} from 'react-native';
+import MapView from 'react-native-maps';
 
 import Component1 from './app/components/Component1/Component1';
 import Component2 from './app/components/Component2/Component2';
@@ -13,22 +14,6 @@ import Component6 from './app/components/Component6/Component6';
 import { Root, Tabs } from './app/config/router';
 
 export default class myapp extends Component {
-
-  // renderScene(route, navigator) {
-  //   switch(route.id) {
-  //     case 'component5':
-  //       return (<Component5 navigator={navigator} title="component5" />)
-  //     case 'component6':
-  //       return (<Component6  navigator={navigator} title="component6" />)
-  //   }
-  // }
-
-// {/*<Navigator*/}
-// {/*initialRoute={{id: 'component5'}}*/}
-// {/*renderScene={this.renderScene}*/}
-// {/*configureScreen={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}*/}
-// {/*/>*/}
-
 // {/*<View>*/}
 // {/*/!*<Component1 />*!/*/}
 // {/*/!*<Component2 />*!/*/}
@@ -38,10 +23,51 @@ export default class myapp extends Component {
 // {/**/}
 // {/*</View>*/}
   render() {
+    const { region } = this.props;
+    console.log(region);
     return(
-      <Root />
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}>
+        </MapView>
+      </View>
     );
   }
 }
+
+// let { width, height } = Dimensions.get('window');
+// const styles = StyleSheet.create({
+//   container: {
+//     ...StyleSheet.absoluteFillObject,
+//     height: 400,
+//     width: 400,
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   map: {
+//     ...StyleSheet.absoluteFillObject,
+//   },
+// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  map: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute'
+  }
+});
 
 AppRegistry.registerComponent('myapp', () => myapp);

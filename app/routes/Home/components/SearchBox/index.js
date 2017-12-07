a1d9f62ff5c01de14ad33f39ff498d975b543e26
Jun <cygoh@umich.edu>
2017-12-07 22:25:50 +0800

    added a drop off search box

--- app/routes/Home/components/SearchBox/index.js
+++ app/routes/Home/components/SearchBox/index.js
@@ -14,6 +14,13 @@ export const SearchBox = ()=> {
           <Input style={styles.inputSearch} placeholder='Choose pick-up location.' />
         </InputGroup>
       </View>
+      <View style={styles.secondInputWrapper}>
+        <Text style={styles.label}>DROP OFF</Text>
+        <InputGroup>
+          <Icon name='search' size={15} color='#FF5E3A'/>
+          <Input style={styles.inputSearch} placeholder='Choose drop-off location.' />
+        </InputGroup>
+      </View>
     </View>
   )
 }

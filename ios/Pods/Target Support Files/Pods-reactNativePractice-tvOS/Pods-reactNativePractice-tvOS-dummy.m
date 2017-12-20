b470148dd2e6ccbd732521fd2d83718f333e9bd0
Jun <cygoh@umich.edu>
2017-12-19 22:46:00 -0800

    setup google places for ios pod

---
+++ ios/Pods/Target Support Files/Pods-reactNativePractice-tvOS/Pods-reactNativePractice-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reactNativePractice_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_reactNativePractice_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-reactNativePractice/Pods-reactNativePractice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reactNativePractice : NSObject
+@end
+@implementation PodsDummy_Pods_reactNativePractice
+@end

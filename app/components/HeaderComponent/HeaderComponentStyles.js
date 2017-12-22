1164fff5e6dd2ea07ee2675aab4d00b9a553c476
Jun <cygoh@umich.edu>
2017-12-21 20:36:18 -0800

    added styles for header

---
+++ app/components/HeaderComponent/HeaderComponentStyles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+const styles = StyleSheet.create({
+  icon: {
+    color: "#fff",
+    fontSize: 20
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 14,
+  },
+  logo: {
+    height: 50,
+    width: 50,
+  }
+})
+
+export default styles;

558f7147e1b703f0ceb05bf9ab271911dec181c6
Jun <cygoh@umich.edu>
2017-12-21 19:36:10 -0800

    added a header

--- app/routes/Home/components/Home.js
+++ app/routes/Home/components/Home.js
@@ -3,6 +3,7 @@ import {AppRegistry, View, Text, StyleSheet, Dimensions} from 'react-native';
 
 import {Container} from 'native-base'
 import MapContainer from './MapContainer';
+import HeaderComponent from '../../../components/HeaderComponent/HeaderComponent';
 
 const {width, height} = Dimensions.get('window');
 
@@ -13,8 +14,14 @@ const LATITUDE_DELTA = 0.020
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
 
 class Home extends React.Component {
-
+  
   componentDidMount() {
+    this.props.region = {
+      latitude: 49.2820,
+      longitude: -123.1171,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    };
     this.props.getCurrentLocation();
   }
 
@@ -25,8 +32,10 @@ class Home extends React.Component {
       latitudeDelta: LATITUDE_DELTA,
       longitudeDelta: LONGITUDE_DELTA,
     }
+
     return(
       <Container>
+        <HeaderComponent />
         {this.props.region.latitude &&
           <MapContainer
             region={this.props.region}
@@ -34,6 +43,7 @@ class Home extends React.Component {
             toggleSearchResultModal={this.props.toggleSearchResultModal}
             getLocationPredictions={this.props.getLocationPredictions}
             resultTypes={this.props.resultTypes}
+            predictions={this.props.predictions}
           />
         }
       </Container>

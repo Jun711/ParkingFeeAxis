1b264ca3c45f0b71558047ea7b53d939dc6fe6b7
Jun <cygoh@umich.edu>
2017-12-23 23:55:52 -0800

    renamed folder to src

--- app/store/reducers.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import {HomeReducer as home} from '../routes/Home/modules/home';
-export const makeRootReducer = () => {
-  return combineReducers({
-    home
-  });
-}
-
-export default makeRootReducer;

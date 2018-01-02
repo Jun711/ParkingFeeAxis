e20961ca3ba3f1070f1fb2823dad65e67b932865
Jun <cygoh@umich.edu>
2018-01-01 22:58:49 -0800

    added an error middleware

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -4,7 +4,13 @@ import makeRootReducer from './reducers';
 import {createLogger} from 'redux-logger';
 
 const log = createLogger({ diff: true, collapsed: true });
-
+const error = (store) => (next) => (action) => {
+  try {
+    next(action);
+  } catch (err) {
+    console.log('error: ', error);
+  }
+}
 export default (initialState = {}) => {
   const middleware = [thunk, log];
   const enhancers = [];
@@ -25,5 +31,8 @@ export default (initialState = {}) => {
     ) // Composes functions from right to left.
   );
 
+  // store.subscribe(() => {
+  //   console.log('store changed', store.getState());
+  // })
   return store;
 }

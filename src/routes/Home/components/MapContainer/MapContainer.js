7fdbb7c7596b4fbd11d0b991790aeeeab7d87875
Jun <cygoh@umich.edu>
2018-01-03 00:02:01 -0800

    cleanup

--- src/routes/Home/components/MapContainer/MapContainer.js
+++ src/routes/Home/components/MapContainer/MapContainer.js
@@ -8,20 +8,6 @@ import styles from './MapContainerStyles';
 
 const parkingSpotPin = require('../../../../assets/parking-icon.png');
 
-// export const MapContainer = ({
-//                                userCoord,
-//                                region,
-//                                displayCentreMarker,
-//                                getInputData,
-//                                toggleSearchResultModal,
-//                                getLocationPredictions,
-//                                resultTypes,
-//                                predictions,
-//                                getSelectedAddress,
-//                                selectedAddress,
-//                                handleRegionChangeComplete,
-//                                nearbyParkingSpots
-//                              }) => {
 export default class MapContainer extends Component {
   constructor(props) {
     super(props);
@@ -49,23 +35,17 @@ export default class MapContainer extends Component {
               <View style={styles.marker}/>
             </View>
           </MapView.Marker.Animated>
-          {/*{displayCentreMarker &&*/}
-          {/*<MapView.Marker.Animated*/}
-          {/*coordinate={region}>*/}
-          {/*<View style={styles.centreMarker} />*/}
-          {/*</MapView.Marker.Animated>*/}
-          {/*}*/}
           {
             this.props.nearbyParkingSpots && this.props.nearbyParkingSpots.map((parkingSpot, index) => {
               return (<MapView.Marker.Animated
                 key={index}
                 coordinate={{
-                  latitude: parkingSpot.geometry.coordinates[1],
-                  longitude: parkingSpot.geometry.coordinates[0]
+                  latitude: parkingSpot.geometry.geometries? parkingSpot.geometry.geometries[0].coordinates[1] : parkingSpot.geometry.coordinates[1],
+                  longitude: parkingSpot.geometry.geometries? parkingSpot.geometry.geometries[0].coordinates[0] : parkingSpot.geometry.coordinates[0]
                 }}
                 title={parkingSpot.properties.description}
-                pinColor='blue'
-                // image={parkingSpotPin}
+                // pinColor='blue'
+                image={parkingSpotPin}
               />)
             })
           }
@@ -86,5 +66,3 @@ export default class MapContainer extends Component {
     )
   }
 }
-
-// export default MapContainer;

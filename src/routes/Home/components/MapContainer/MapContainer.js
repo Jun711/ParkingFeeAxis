d87c864a3135258f6ed7507c78e2ed88197c3879
Jun <cygoh@umich.edu>
2018-01-02 23:21:46 -0800

    converted MapContainer to a React component

--- src/routes/Home/components/MapContainer/MapContainer.js
+++ src/routes/Home/components/MapContainer/MapContainer.js
@@ -8,75 +8,83 @@ import styles from './MapContainerStyles';
 
 const parkingSpotPin = require('../../../../assets/parking-icon.png');
 
-export const MapContainer = ({
-                               userCoord,
-                               region,
-                               displayCentreMarker,
-                               getInputData,
-                               toggleSearchResultModal,
-                               getLocationPredictions,
-                               resultTypes,
-                               predictions,
-                               getSelectedAddress,
-                               selectedAddress,
-                               handleRegionChangeComplete,
-                               nearbyParkingSpots
-                             }) => {
-  return (
-    <View style={styles.container}>
-      <MapView
-        showsUserLocation={false}
-        showsMyLocationButton={true}
-        loadingEnabled={true}
-        loadingIndicatorColor={'#746855'}
-        provider={MapView.PROVIDER_GOOGLE}
-        style={styles.map}
-        region={region}
-        onRegionChangeComplete={(event) => handleRegionChangeComplete(event)}
-        customMapStyle={styles.mapStyle}
-      >
-        <MapView.Marker.Animated
-          coordinate={userCoord}
-          pinColor='blue'>
-          <View style={styles.radius}>
-            <View style={styles.marker}/>
-          </View>
-        </MapView.Marker.Animated>
-        {/*{displayCentreMarker &&*/}
-        {/*<MapView.Marker.Animated*/}
-        {/*coordinate={region}>*/}
-        {/*<View style={styles.centreMarker} />*/}
-        {/*</MapView.Marker.Animated>*/}
-        {/*}*/}
-        {
-          nearbyParkingSpots && nearbyParkingSpots.map((parkingSpot, index) => {
-            return (<MapView.Marker.Animated
-              key={index}
-              coordinate={{
-                latitude: parkingSpot.geometry.coordinates[1],
-                longitude: parkingSpot.geometry.coordinates[0]
-              }}
-              title={parkingSpot.properties.description}
-              pinColor='blue'
-              // image={parkingSpotPin}
-            />)
-          })
+// export const MapContainer = ({
+//                                userCoord,
+//                                region,
+//                                displayCentreMarker,
+//                                getInputData,
+//                                toggleSearchResultModal,
+//                                getLocationPredictions,
+//                                resultTypes,
+//                                predictions,
+//                                getSelectedAddress,
+//                                selectedAddress,
+//                                handleRegionChangeComplete,
+//                                nearbyParkingSpots
+//                              }) => {
+export default class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    console.log('this.props: ', props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          showsUserLocation={false}
+          showsMyLocationButton={true}
+          loadingEnabled={true}
+          loadingIndicatorColor={'#746855'}
+          provider={MapView.PROVIDER_GOOGLE}
+          style={styles.map}
+          region={this.props.region}
+          onRegionChangeComplete={(event) => this.props.handleRegionChangeComplete(event)}
+          customMapStyle={styles.mapStyle}
+        >
+          <MapView.Marker.Animated
+            coordinate={this.props.userCoord}
+            pinColor='blue'>
+            <View style={styles.radius}>
+              <View style={styles.marker}/>
+            </View>
+          </MapView.Marker.Animated>
+          {/*{displayCentreMarker &&*/}
+          {/*<MapView.Marker.Animated*/}
+          {/*coordinate={region}>*/}
+          {/*<View style={styles.centreMarker} />*/}
+          {/*</MapView.Marker.Animated>*/}
+          {/*}*/}
+          {
+            this.props.nearbyParkingSpots && this.props.nearbyParkingSpots.map((parkingSpot, index) => {
+              return (<MapView.Marker.Animated
+                key={index}
+                coordinate={{
+                  latitude: parkingSpot.geometry.coordinates[1],
+                  longitude: parkingSpot.geometry.coordinates[0]
+                }}
+                title={parkingSpot.properties.description}
+                pinColor='blue'
+                // image={parkingSpotPin}
+              />)
+            })
+          }
+        </MapView>
+        <SearchBox
+          getInputData={this.props.getInputData}
+          toggleSearchResultModal={this.props.toggleSearchResultModal}
+          getLocationPredictions={this.props.getLocationPredictions}
+          selectedAddress={this.props.selectedAddress}
+        />
+        {(this.props.resultTypes.pickUp || this.props.resultTypes.dropOff) &&
+        <SearchResults
+          predictions={this.props.predictions}
+          getSelectedAddress={this.props.getSelectedAddress}
+        />
         }
-      </MapView>
-      <SearchBox
-        getInputData={getInputData}
-        toggleSearchResultModal={toggleSearchResultModal}
-        getLocationPredictions={getLocationPredictions}
-        selectedAddress={selectedAddress}
-      />
-      {(resultTypes.pickUp || resultTypes.dropOff) &&
-      <SearchResults
-        predictions={predictions}
-        getSelectedAddress={getSelectedAddress}
-      />
-      }
-    </View>
-  )
+      </View>
+    )
+  }
 }
 
-export default MapContainer;
+// export default MapContainer;

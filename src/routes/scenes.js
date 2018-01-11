c1030acc22a004c4c6f652dce710d3ea86a97b12
Jun <cygoh@umich.edu>
2018-01-10 21:58:50 -0800

    feat(scenes): add a FAQ scene

--- src/routes/scenes.js
+++ src/routes/scenes.js
@@ -1,12 +1,26 @@
-import React from "react";
-import {Actions, Scene} from "react-native-router-flux";
-import HomeContainer from "./Home/container/HomeContainer";
+import React from 'react';
+import { Actions, Scene, Stack } from 'react-native-router-flux';
+import HomeContainer from './Home/container/HomeContainer';
+import AboutContainer from './About/container/AboutContainer';
+import FaqContainer from './Faq/container/FaqContainer';
 
 const scenes = Actions.create(
-  <Scene key="root" hideNavBar>
-    <Scene key="home" component={HomeContainer} title="home" initial />
-  </Scene>
-
+  <Stack
+    hideNavBar
+    key='root'
+    titleStyle={{alignSelf: 'center'}}
+  >
+    <Scene key='home' component={HomeContainer} title='home' initial/>
+    <Scene hideNavBar key='about' component={AboutContainer} title='about' />
+    <Scene
+      hideNavBar={false}
+      key='faq'
+      component={FaqContainer}
+      title='FAQ'
+      titleStyle={{alignSelf: 'flex-start'}}
+      back={true}
+    />
+  </Stack>
 );
 
 export default scenes;

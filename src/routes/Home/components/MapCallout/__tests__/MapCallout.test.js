a7b03f6e02b23c2ae7cf8d431941ef54a8b1fdc6
Jun <cygoh@umich.edu>
2018-01-11 18:04:29 -0800

    updated MapCallout test snapshot

---
+++ src/routes/Home/components/MapCallout/__tests__/MapCallout.test.js
@@ -0,0 +1,16 @@
+import 'react-native';
+import React from 'react';
+import MapCallout from '../MapCallout';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+describe('<MapCallout />', () => {
+  it('MapCallout renders correctly', () => {
+    const mapCalloutTree = renderer.create(
+      <MapCallout />
+    ).toJSON();
+    expect(mapCalloutTree).toMatchSnapshot();
+  });
+});
+

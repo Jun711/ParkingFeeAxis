e08e8a93331e92be34fc56a231c451b7aa12e897
Jun <cygoh@umich.edu>
2018-01-12 17:14:55 -0800

    refactor(FaqList mock data): update mock data

--- src/routes/Faq/components/FaqList/FaqData.js
+++ src/routes/Faq/components/FaqList/FaqData.js
@@ -1,15 +1,45 @@
 export default FaqData = [
   {
     key: '1',
-    text: 'There are three types of parking flags. Green is the cheapest, ' +
-    'yellow is the average while red is the most expensive nearby parking rate.'
+    text: 'This app allows you to find nearby meter parking spot by searching for ' +
+      'a location or move the center flag to a location.'
   },
   {
     key: '2',
-    text: 'We don\'t keep your location data.'
+    text: 'The markers / flags on the map indicate a meter parking spot.'
   },
   {
     key: '3',
-    text: 'The data comes from Vancouver Open Data Catalogue.'
+    text: 'There are three types of parking flags. Green is the cheapest, ' +
+    'yellow is the average while red is the most expensive nearby parking meter rate.'
+  },
+  {
+    key: '4',
+    text: 'If you pan around the map, you will discover parking meter around a center point.'
+  },
+  {
+    key: '5',
+    text: 'This app parking meter data comes from Vancouver Open Data Catalogue.'
+  },
+  {
+    key: '6',
+    text: 'This app requires location permission and gps to work.'
+  },
+  {
+    key: '7',
+    text: 'If no parking spots show up nearby a specific location, it may mean ' +
+      'that there is no meter parkings nearby.'
+  },
+  {
+    key: '8',
+    text: 'Your location data is not collected by this app.'
+  },
+  {
+    key: '9',
+    text: 'If you find any bug or anomaly, feel free to contact me at ninja@ninja.com.'
+  },
+  {
+    key: '10',
+    text: 'If you would like to see more features, feel free to contact me at ninja@ninja.com.'
   },
 ]

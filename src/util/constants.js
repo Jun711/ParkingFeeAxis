5079bd1cae39c2a96ecc8db5d7fcc38d92fc8efe
Jun <cygoh@umich.edu>
2018-01-12 20:09:03 -0800

    refactor(constants): add defaultAjaxTimeout

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,21 +1,23 @@
+// config
 export const LATITUDE_DELTA = 0.010;
+export const defaultAjaxTimeout = 30000;
 
-// Location permission constants
+// location permission constants
 export const PERMISSION_REQ_TITLE = 'Parking App Location Permission';
 export const PERMISSION_REQ_MSG = 'Parking App needs access to your location to locate nearby parking.';
 
-// Toast
+// toast
 export const TOAST_HEADER_Y_OFFSET = 145;
 export const TOAST_HEADER_X_OFFSET = 0;
 export const LOCATION_ERROR_MSG_ONE = 'Please enable location service.';
 export const LOCATION_ERROR_MSG_TWO = 'Location service is unavailable.';
 export const LOCATION_ERROR_DEFAULT_MSG = 'Location request timeout.';
 
-// About
+// about
 export const APP_NAME = 'Ninja Hideout';
 export const APP_VERSION = 'Version 1.0.0';
 
-// Present
+// time
 export const WEEKDAY_9_6 = 'weekdayOfficeHourRate';
 export const WEEKDAY_6_PLUS = 'weekdayAfterOfficeRate';
 export const SAT_9_6 = 'satOfficeHourRate';
@@ -24,10 +26,12 @@ export const SUN_9_6 = 'sunOfficeHourRate';
 export const SUN_6_PLUS = 'sunAfterOfficeRate';
 export const FREE_PARKING = 'freeParking';
 
-// Rate
+// min and max rate
 export const MIN_PARKING_RATE = 100;
 export const MAX_PARKING_RATE = -1;
 
-// Color
-export const LOADER_COLOR = '#515c6d';
+// color
+export const LOADER_COLOR = '#515c6d'
+
+
 

4acabac582c9d7ee6b579260d2856553d60a1969
Jun <cygoh@umich.edu>
2018-01-12 16:26:13 -0800

    test(faq module): add tests for LOAD_FAQ_LIST action

--- src/routes/Faq/modules/__tests__/faq.test.js
+++ src/routes/Faq/modules/__tests__/faq.test.js
@@ -1,14 +1,43 @@
-import { loadFaqList } from '../faq';
-import constants from '../actionConstants';
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
 
-const { LOAD_FAQ_LIST } = constants;
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
 
-it('creates a LOAD_FAQ_LIST action', () => {
-  expect(loadFaqList()).toEqual(
-    {
+const store = mockStore({})
+import { loadFaqList } from '../faq'
+import constants from '../actionConstants'
+
+const {LOAD_FAQ_LIST, LOAD_FAQ_LIST_ERROR} = constants
+
+beforeEach(() => {
+  store.clearActions()
+})
+
+describe('<FaqList />', () => {
+  it(`should handle ${LOAD_FAQ_LIST} failure action`, async () => {
+    const mockError = new Error('error')
+    fetch.mockResponseFailure(mockError)
+    await store.dispatch(loadFaqList('/test'))
+
+    const actions = await store.getActions()
+    expect(actions[0]).toEqual({
+      type: LOAD_FAQ_LIST_ERROR,
+      payload: mockError
+    })
+    expect(store.getActions()).toMatchSnapshot()
+  })
+
+  it(`should handle ${LOAD_FAQ_LIST} success action`, async () => {
+    const response = '{"faqs": [{"text": "Frequently asked questions"}]}'
+    fetch.mockResponseSuccess(response)
+    await store.dispatch(loadFaqList('/test'))
+
+    const actions = await store.getActions()
+    expect(actions[0]).toEqual({
       type: LOAD_FAQ_LIST,
-    }
-  );
-  // using Jest snapshot
-  expect(loadFaqList()).toMatchSnapshot();
-});
+      payload: JSON.parse(response)
+    })
+    expect(store.getActions()).toMatchSnapshot()
+  })
+})

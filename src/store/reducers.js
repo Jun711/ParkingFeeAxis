b9597d7a84d50436661145266a9ca5e35d8e84f9
Jun <cygoh@umich.edu>
2018-01-12 17:48:37 -0800

    feat(reducer): update reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { HomeReducer as home } from '../routes/Home/modules/home';
 import { AboutReducer as about } from '../routes/About/modules/about';
+import { FaqReducer as faq } from '../routes/Faq/modules/faq';
 
 export const makeRootReducer = () => {
   return combineReducers({
     home,
-    about
+    about,
+    faq
   });
 }
 

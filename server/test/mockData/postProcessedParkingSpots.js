40c2e57203047ede8b4d447a3b62dc779ee090cf
Jun <cygoh@umich.edu>
2018-01-13 17:17:59 -0800

    test(parkingSpotUtil): test parkingSpotUtil processDescription utils

---
+++ server/test/mockData/postProcessedParkingSpots.js
@@ -0,0 +1,32 @@
+exports.parkingSpots = [
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        -123.116864034159,
+        49.2819351255461,
+        0
+      ]
+    },
+    "properties": {
+      "name": "Meter Head Type: \nSingle Motorbike",
+      "styleUrl": "#ParkingMeterStyler_KMLStyler_2.50",
+      "styleHash": "9ccb819",
+      "description": "Meter Id: 600698\nRates: \nM-F 9AM to 6PM: $2.50\nM-F 6PM to 10PM: $2.00\nSat 9AM to 6PM: $2.50\nSat 6PM to 10PM: $2.00\nSun 9AM to 6PM: $2.50\nSun 6PM to 10PM: $2.00\n     Miscellaneous: $2.50\nEffect: 9:00 AM TO 10:00 PM\nTime Limits: \nM-F 9AM to 6PM: No Time Limit\nM-F 6PM to 10PM: No Time Limit\nSat 9AM to 6PM: No Time Limit\nSat 6PM to 10PM:No Time Limit\nSun 9AM to 6PM: No Time Limit\nSun 6PM to 10PM: No Time Limit\n     Miscellaneous: No Time Limit\nParking Prohibitions: AM PM\nCredit Card Enabled: No\nPay by Phone Number: 65619",
+      "stroke": "#fde4ff",
+      "stroke-opacity": 1,
+      "stroke-width": 2,
+      "weekdayOfficeHourRate": "2.50",
+      "weekdayAfterOfficeRate": "2.00",
+      "satOfficeHourRate": "2.50",
+      "satAfterOfficeRate": "2.00",
+      "sunOfficeHourRate": "2.50",
+      "sunAfterOfficeRate": "2.00",
+      "freeParking": "0"
+    },
+    "id": "9013"
+  }
+]
+
+
---
+++ server/test/mockData/preProcessedParkingSpots.js
@@ -0,0 +1,24 @@
+exports.parkingSpots = [
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        -123.116864034159,
+        49.2819351255461,
+        0
+      ]
+    },
+    "properties":
+      {
+        "name": "Meter Head Type: \nSingle Motorbike",
+        "styleUrl": "#ParkingMeterStyler_KMLStyler_2.50",
+        "styleHash": "9ccb819",
+        "description": "<br>Meter Id: 600698\n<br>Rates: \n<br>M-F 9AM to 6PM: $2.50\n<br>M-F 6PM to 10PM: $2.00\n<br>Sat 9AM to 6PM: $2.50\n<br>Sat 6PM to 10PM: $2.00\n<br>Sun 9AM to 6PM: $2.50\n<br>Sun 6PM to 10PM: $2.00\n<br>     Miscellaneous: $2.50\n<br>Effect: 9:00 AM TO 10:00 PM\n<br>Time Limits: \n<br>M-F 9AM to 6PM: No Time Limit\n<br>M-F 6PM to 10PM: No Time Limit\n<br>Sat 9AM to 6PM: No Time Limit\n<br>Sat 6PM to 10PM:No Time Limit\n<br>Sun 9AM to 6PM: No Time Limit\n<br>Sun 6PM to 10PM: No Time Limit\n<br>     Miscellaneous: No Time Limit\n<br>Parking Prohibitions: AM PM<br>Credit Card Enabled: No<br>Pay by Phone Number: 65619\n",
+        "stroke": "#fde4ff",
+        "stroke-opacity": 1,
+        "stroke-width": 2
+      },
+    "id": "9013"
+  }
+]

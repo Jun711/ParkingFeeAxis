5ef19b790af05c49c3023756e9eefef779a90d64
Jun <cygoh@umich.edu>
2018-01-15 17:05:40 -0800

    refactor(constants):

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,37 +1,44 @@
 // config
-export const LATITUDE_DELTA = 0.010;
-export const defaultAjaxTimeout = 30000;
+export const LATITUDE_DELTA = 0.010
+export const defaultAjaxTimeout = 30000
+export const DEFAULT_LATITUDE = 49.2820
+export const DEFAULT_LONGITUDE = -123.1171
 
 // location permission constants
-export const PERMISSION_REQ_TITLE = 'Parking App Location Permission';
-export const PERMISSION_REQ_MSG = 'Parking App needs access to your location to locate nearby parking.';
+export const PERMISSION_REQ_TITLE = 'Parking App Location Permission'
+export const PERMISSION_REQ_MSG = 'Parking App needs access to your location to locate nearby parking.'
 
 // toast
-export const TOAST_HEADER_Y_OFFSET = 145;
-export const TOAST_HEADER_X_OFFSET = 0;
-export const LOCATION_ERROR_MSG_ONE = 'Please enable location service.';
-export const LOCATION_ERROR_MSG_TWO = 'Location service is unavailable.';
-export const LOCATION_ERROR_DEFAULT_MSG = 'Location request timeout.';
+export const TOAST_HEADER_Y_OFFSET = 145
+export const TOAST_HEADER_X_OFFSET = 0
+export const LOCATION_ERROR_MSG_ONE = 'Please enable location service.'
+export const LOCATION_ERROR_MSG_TWO = 'Location service is unavailable.'
+export const LOCATION_ERROR_DEFAULT_MSG = 'Location request timeout.'
 
 // about
-export const APP_NAME = 'Ninja Hideout';
-export const APP_VERSION = 'Version 1.0.0';
+export const APP_NAME = 'Path'
+export const APP_VERSION = 'Version 1.0.0'
 
 // time
-export const WEEKDAY_9_6 = 'weekdayOfficeHourRate';
-export const WEEKDAY_6_PLUS = 'weekdayAfterOfficeRate';
-export const SAT_9_6 = 'satOfficeHourRate';
-export const SAT_6_PLUS = 'satAfterOfficeRate';
-export const SUN_9_6 = 'sunOfficeHourRate';
-export const SUN_6_PLUS = 'sunAfterOfficeRate';
-export const FREE_PARKING = 'freeParking';
+export const WEEKDAY_9_6 = 'weekdayOfficeHourRate'
+export const WEEKDAY_6_PLUS = 'weekdayAfterOfficeRate'
+export const SAT_9_6 = 'satOfficeHourRate'
+export const SAT_6_PLUS = 'satAfterOfficeRate'
+export const SUN_9_6 = 'sunOfficeHourRate'
+export const SUN_6_PLUS = 'sunAfterOfficeRate'
+export const FREE_PARKING = 'freeParking'
 
 // min and max rate
-export const MIN_PARKING_RATE = 100;
-export const MAX_PARKING_RATE = -1;
+export const MIN_PARKING_RATE = 100
+export const MAX_PARKING_RATE = -1
 
 // color
 export const LOADER_COLOR = '#515c6d'
+
+// search bar
+export const SEARCH_PLACEHOLDER = 'Search'
+export const SEARCH_INPUT_KEY = 'headerSearchBar'
+
 
 
 

e0e0207cc188aa978af7d81c5bdb3b160c5375fa
Jun <cygoh@umich.edu>
2018-01-20 19:46:59 -0800

    style(GetLocationButton):

--- src/routes/Home/components/GetLocationButton/GetLocationButton.js
+++ src/routes/Home/components/GetLocationButton/GetLocationButton.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import { View, Button } from 'native-base'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
-import styles from './FloatingActionButtonStyles'
+import styles from './GetLocationButtonStyles'
 
-export default class FloatingActionButton extends Component {
+export default class GetLocationButton extends Component {
   render() {
     return (
       <View>
-        <Button style={styles.fabContainer} onPress={() => this.props.getCurrentLocation()}>
+        <Button style={styles.container} onPress={() => this.props.getCurrentLocation()}>
           <Icon name='crosshairs' style={styles.icon}/>
         </Button>
       </View>
--- src/routes/Home/components/GetLocationButton/GetLocationButtonStyles.js
+++ src/routes/Home/components/GetLocationButton/GetLocationButtonStyles.js
@@ -1,16 +1,16 @@
 import { StyleSheet } from 'react-native'
-import { THEME_COLOR } from '../../../../util/constants'
+import { THEME_COLOR, WHITE_COLOR, TITLE_FONT_SIZE } from '../../../../util/constants'
 
 const styles =  StyleSheet.create({
-  fabContainer: {
-    borderColor: '#fff',
-    height: 30,
-    width: 30,
-    borderRadius: 30 / 2,
+  container: {
+    borderColor: WHITE_COLOR,
+    height: 40,
+    width: 40,
+    borderRadius: 40 / 2,
     justifyContent: 'center',
     position: 'absolute',
-    bottom: 100,
-    right: 20,
+    bottom: 50,
+    right: 30,
     shadowColor: '#000',
     shadowOpacity: 0.8,
     shadowRadius: 2,
@@ -18,12 +18,12 @@ const styles =  StyleSheet.create({
       height: 1,
       width: 0
     },
-    backgroundColor: '#fff',
+    backgroundColor: WHITE_COLOR,
     alignItems: 'center',
   },
   icon: {
     color: THEME_COLOR,
-    fontSize: 20
+    fontSize: TITLE_FONT_SIZE
   }
 })
 

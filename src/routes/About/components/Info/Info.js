90a38a8fce989efb6d7dc1b540f6d820c7e440ef
Jun <cygoh@umich.edu>
2018-01-20 19:38:47 -0800

    refactor(About): use onPressIn to initiate server call earlier

--- src/routes/About/components/Info/Info.js
+++ src/routes/About/components/Info/Info.js
@@ -1,34 +1,37 @@
-import React, { Component } from 'react';
-import { Text, View, SectionList } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import InfoData from './InfoData';
-import SeparatorComponent from '../../../../components/SeparatorComponent/SeparatorComponent';
+import React, { Component } from 'react'
+import { Text, View, SectionList, TouchableNativeFeedback } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import InfoData from './InfoData'
+import SeparatorComponent from '../../../../components/SeparatorComponent/SeparatorComponent'
 
-import styles from './InfoStyles';
+import styles from './InfoStyles'
 
 export default class Info extends Component {
+  _renderItem = ({item}) => (
+    <TouchableNativeFeedback
+      onPressIn={(event) => this.props.openPage({
+        event: event.nativeEvent,
+        page: item.key
+      })}
+      background={TouchableNativeFeedback.SelectableBackground()}
+    >
+      <View key={item.key} style={styles.itemContainer}>
+        <View style={styles.iconContainer}>
+          <Icon style={styles.infoIcon} name={item.icon}/>
+        </View>
+        <Text style={styles.infoItem}>{item.name}</Text>
+      </View>
+    </TouchableNativeFeedback>)
+
+  _renderSectionHeader = ({section}) => <Text style={styles.sectionTitle}>{section.sectionTitle}</Text>
 
   render() {
-    console.log('InfoData: ', InfoData)
     return (
       <View style={styles.container}>
         <SectionList
           sections={InfoData}
-          renderItem={
-            ({item}) => (
-              <View key={item.key} style={styles.itemContainer}>
-                <View style={styles.iconContainer}>
-                  <Icon style={styles.infoIcon} name={item.icon}/>
-                </View>
-                <Text style={styles.infoItem} onPress={(event) => this.props.openPage({
-                  event: event.nativeEvent,
-                  page: item.key
-                })}>{item.name}</Text>
-              </View>)
-          }
-          renderSectionHeader={
-            ({section}) => <Text style={styles.sectionTitle}>{section.sectionTitle}</Text>
-          }
+          renderItem={this._renderItem}
+          renderSectionHeader={this._renderSectionHeader}
           ItemSeparatorComponent={() => <SeparatorComponent/>}
         />
       </View>

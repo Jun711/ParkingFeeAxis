5554737b5c735485c3d913d0902f063062764e3d
Jun <cygoh@umich.edu>
2018-01-23 23:08:14 -0800

    chore(use prod adId):

--- src/components/BottomBannerAd/BottomBannerAd.js
+++ src/components/BottomBannerAd/BottomBannerAd.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { AdMobBanner } from 'react-native-admob'
 
-//ca-app-pub-6805425741453021/2887602580
+// ca-app-pub-6805425741453021/2887602580 // prod
+// ca-app-pub-3940256099942544/6300978111
 export const BottomBannerAd = () => {
   return (
     <AdMobBanner
       adSize='smartBannerPortrait'
-      adUnitID='ca-app-pub-3940256099942544/6300978111'
-      testDevices={[AdMobBanner.simulatorId]}
+      adUnitID='ca-app-pub-6805425741453021/2887602580'
       onAdFailedToLoad={error => console.error(error)}
     />
   );

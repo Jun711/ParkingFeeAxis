1438454a70960f4409a69ae2bfa207cc7d4ac5f2
Jun <cygoh@umich.edu>
2018-02-06 12:01:28 -0800

    enable admob

--- android/app/src/main/java/com/jungoh/MainActivity.java
+++ android/app/src/main/java/com/jungoh/MainActivity.java
@@ -2,6 +2,7 @@ package com.jungoh;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends ReactActivity {
 
@@ -9,6 +10,8 @@ public class MainActivity extends ReactActivity {
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this, true);  // here
         super.onCreate(savedInstanceState);
+
+        MobileAds.initialize(this, "ca-app-pub-6805425741453021~7240173305");
     }
     /**
      * Returns the name of the main component registered from JavaScript.
